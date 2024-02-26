a487aed1cea2455ac6b21cc73582699834873c70
agl5702 <137901683+agl5702@users.noreply.github.com>
2024-02-26 16:56:35 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,4 +5,6 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
+python manage.py createsuperuser --noinput --username adminwinadmin --email admin@example.com 
+
 python manage.py migrate
