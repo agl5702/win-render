33d28e48f535eaae88176395b1855b90dd37fc60
agl5702 <137901683+agl5702@users.noreply.github.com>
2024-02-26 18:47:15 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,12 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input 
 python manage.py migrate
-python manage.py createsuperuser --username admin
+
+# Generar una contraseña aleatoria
+password=$(openssl rand -base64 12)
+
+# Crear superusuario y establecer la contraseña generada
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin2', 'admin@example.com', '$password')" | python manage.py shell
 
 
 
