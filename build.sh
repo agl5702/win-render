0d6c3cac974086b0c22aab65866da3557ddd972b
agl5702 <137901683+agl5702@users.noreply.github.com>
2024-03-03 18:49:33 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input 
 python manage.py migrate
-python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('adminangel', '', 'agl5702')"
+#python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('adminangel', '', 'agl5702')"
 
 
 
