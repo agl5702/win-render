6e721b11ec0226a20f4108927608daf582b4fd99
agl5702 <advasquez41@misena.edu.co>
2024-03-13 01:45:25 -0500

    win update

--- partidos_horario/models.py
+++ partidos_horario/models.py
@@ -3,13 +3,6 @@ from equipo_jugador.models import Equipo
 
 # Create your models here.
 
-class Horario(models.Model):
-    fecha= models.DateField()
-    hora= models.TimeField()
-    detalles= models.CharField(max_length=100)
-    
-    def __str__(self):
-        return f'Fecha: {self.fecha}'
 
 
 
@@ -22,11 +15,6 @@ class Partido(models.Model):
     fecha= models.DateField()
     hora= models.TimeField()
 
-    horario_partido= models.ForeignKey(Horario, on_delete=models.CASCADE)
-    gol_local= models.IntegerField(default=0)
-    gol_visitante= models.IntegerField(default=0)
-    detalle= models.TextField()
-    
 
     def __str__(self):
         return f'{self.gol_local} {self.gol_visitante}'
--- partidos_horario/views.py
+++ partidos_horario/views.py
@@ -1,21 +1,11 @@
 
 # Create your views here.
 from rest_framework import viewsets
-<<<<<<< HEAD
+
 from .serializer import PartidoSerializer
 from .models import Partido
 
-# Vista crud.. 
 
-=======
-from .serializer import HorarioSerializer,PartidoSerializer
-from .models import Horario, Partido
-
-# Vista crud.. 
-class HorarioView(viewsets.ModelViewSet):
-    serializer_class = HorarioSerializer
-    queryset = Horario.objects.all()
->>>>>>> 0d6c3cac974086b0c22aab65866da3557ddd972b
     
 class PartidoView(viewsets.ModelViewSet):
     serializer_class = PartidoSerializer
