ac2e3ff10c183ad7719395fa7e611d90153352e4
agl5702 <advasquez41@misena.edu.co>
2024-04-01 21:48:29 -0500

    backend  commit

--- partidos_horario/models.py
+++ partidos_horario/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 from equipo_jugador.models import Equipo
 from django.conf import settings
-from django.contrib.auth import get_user_model
-User = get_user_model()
+from users.models import UserManager
+
 # Create your models here.
 
 
 
 
 class Partido(models.Model):
-    usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,default=User.objects.get_default_user)
+    usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,default=UserManager().get_default_user)
     equipo_local = models.ManyToManyField(Equipo, related_name='equipos_locales')
     equipo_visitante = models.ManyToManyField(Equipo, related_name='equipos_visitantes')
 
--- torneos/models.py
+++ torneos/models.py
@@ -5,8 +5,9 @@ from django.conf import settings
 from django.db import models
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
-from django.contrib.auth import get_user_model
-User = get_user_model()
+from users.models import UserManager
+
+
 
 # modelo
 class Torneo(models.Model):
@@ -14,7 +15,7 @@ class Torneo(models.Model):
     titulo= models.CharField(max_length=200)
     descripcion= models.TextField()
     fecha_inicio= models.DateField(auto_now_add=True)
-    usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,default=User.objects.get_default_user)
+    usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,default=UserManager().get_default_user)
 
     def get_context_data(self, **kwargs):
         # Llama al método de la clase base para obtener el contexto inicial
