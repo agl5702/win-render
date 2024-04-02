c6b0cb7a7aff680eb1f0fe386497b9bbb984e2b3
agl5702 <advasquez41@misena.edu.co>
2024-04-01 21:07:30 -0500

    backend  commit

--- equipo_jugador/models.py
+++ equipo_jugador/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from torneos.models import Torneo
-from users.models import User
 from django.conf import settings
+from django.contrib.auth import get_user_model
+
 
 # Create your models here.
 class Equipo(models.Model):
-    usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,default=User.objects.get_default_user)
+    usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,default=get_user_model().objects.get_default_user)
     nombre= models.CharField(max_length=45)
     logo= models.ImageField(upload_to='equipo_jugador/media/equipo',blank=True,null=True)
     torneo_equipo=models.ForeignKey(Torneo, on_delete=models.CASCADE,default=1)
@@ -31,7 +32,7 @@ class Equipo(models.Model):
 
 class Jugador(models.Model):
 
-    usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,default=User.objects.get_default_user)
+    usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,default=get_user_model().objects.get_default_user)
     id_jugador= models.IntegerField(primary_key=True)
     jugador_equipo= models.ManyToManyField(Equipo, related_name='jugadores') 
     nombre= models.CharField(max_length=45)
@@ -56,7 +57,7 @@ class Jugador(models.Model):
         return f'Nombre: {self.nombre} {self.jugador_equipo}'
 
 class Equipo_torneo(models.Model):
-    usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,default=User.objects.get_default_user)
+    usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,default=get_user_model().objects.get_default_user)
     equipo= models.ForeignKey(Equipo, on_delete=models.CASCADE)
 
     def get_context_data(self, **kwargs):
