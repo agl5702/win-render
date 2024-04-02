bb75afb25734832268598ff438f9b3985e3acb97
agl5702 <137901683+agl5702@users.noreply.github.com>
2024-04-01 20:46:39 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,9 +5,19 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input 
+python manage.py makemigrations users
+python manage.py makemigrations torneos
+python manage.py makemigrations equipo_jugador
+python manage.py makemigrations partidos_horario
+python manage.py makemigrations tabla_posiciones
 python manage.py migrate
 
 #python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('tenshidesu12345@gmail.com', '', 'agl57022023')"
+
+
+
+
+
 
 
 
